b1a5b8df6abbd25c500f2e35a4e0299cffb3c201
thirtyjohn <thirtyjohn1987@gmail.com>
2013-05-11 08:43:30 +0800

    cps

---
+++ helpers/cpslinks.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+
+
+
+class Cpslink:
+    def __init__(self):
+        pass
+    def encode(self):
+        pass
+    def decode(self):
+        pass
+
+class Tblink(Cpslink):
+    def encode(self):
+        pass
+    def decode(self):
+        pass
