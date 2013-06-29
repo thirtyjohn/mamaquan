5afd32dccba506bb80aeb16591e504ac017b5891
thirtyjohn <thirtyjohn1987@gmail.com>
2013-06-29 16:51:50 +0800

    nginx and uwsgi and application

---
+++ application.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import web
+
+urls = (
+    "/","webapp.controllers.browse.index",
+    "/shopping/(\d+)","webapp.controllers.browse.shoppingitem",
+    "/shopping/(\d+)/delete","webapp.controllers.action.delitem",
+    "/mulu","webapp.controllers.browse.mulu",
+    "/(naifen|niaobu|wanju|baojian)/s","webapp.controllers.browse.productsearch",
+    "/(naifen|niaobu|wanju|baojian)/(\d+)","webapp.controllers.browse.product",
+)
+
+
+app = web.application(urls,globals())
+application = app.wsgifunc()
