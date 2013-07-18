443a86458d22b23b19bdaece2c145a99d8b9b0d1
thirtyjohn <thirtyjohn1987@gmail.com>
2013-07-18 21:21:47 +0800

    execpt

--- manager/danpings/dpcheck.py
+++ manager/danpings/dpcheck.py
@@ -1,6 +1,8 @@
 #coding:utf8
 from manager.settings import dbconn
 from helpers.b2c import factory
+from helpers.loggers import get_logger
+import traceback
 
 def getDpForCheck():
     return dbconn.query("select * from (select * from formaldanping order by id desc limit 100)t where t.stock = 1")
@@ -21,6 +23,9 @@ def check(dp):
 def main():
     dps = getDpForCheck()
     for dp in dps:
-        stock = check(dp)
-        if not stock:
-            dbconn.update("formaldanping",where="id=$dpid",vars=dict(dpid=dp.ID),stock=0,syn=2)
+        try:
+            stock = check(dp)
+            if not stock:
+                dbconn.update("formaldanping",where="id=$dpid",vars=dict(dpid=dp.ID),stock=0,syn=2)
+        except:
+            get_logger("general").debug(traceback.format_exc())
